37392ea1603e14dc7b931bc22ef885ccc6b0b899
Sem <931684+sembrestels@users.noreply.github.com>
2024-09-09 02:08:52 +0200

    Add extra config to next

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   transpilePackages: ["@repo/ui"],
   distDir: "build",
   output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
