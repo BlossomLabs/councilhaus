4e7be004b8a876d5e0313182f1b02c19f475ee5d
Sem <931684+sembrestels@users.noreply.github.com>
2024-11-05 19:51:17 +0700

    Add opengraph tags

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SITE_NAME } from "../../../../constants";
 import { ConnectButton } from "./ConnectButton";
 
 export default function Header() {
@@ -6,9 +7,9 @@ export default function Header() {
     <div className="bg-gray-900 py-4">
       <div className="container max-w-4xl mx-auto flex justify-between items-center">
         <div className="flex items-center gap-2">
-          <img src="/logo-light.svg" alt="Council Haus" className="h-10" />
-          <h1 className="text-xl font-bold text-accent tracking-wider">
-            Council Haus
+          <img src="/logo-dark.svg" alt="CouncilHaus" className="h-9" />
+          <h1 className="text-xl font-medium text-accent tracking-wider mt-1">
+            {SITE_NAME}
           </h1>
         </div>
         <ConnectButton />
